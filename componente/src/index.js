a0dd243f1c1047338b75649f2c25fc13d22d74ce
eduardoha8585 <eduardoha8585@gmail.com>
2024-01-16 18:11:36 -0500

    añadiendo cambios en archivo index.js

--- componente/src/index.js
+++ componente/src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const App = () => {
-  return <><p>Hello World</p></>
+  const now = new Date()
+  const a = 10
+  const b = 20
+  console.log(now, a+b)
+  return (
+    <>
+      <p>Hello World, it is {now.toString()}</p>
+      <p>{a} plus {b} is {a + b}</p>
+    </>
+  )
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+export default App
