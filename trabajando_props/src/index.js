deac7a7b647c339e2c2fbac3cd3663f7b34bfce6
eduardoha8585 <eduardoha8585@gmail.com>
2024-01-17 15:32:39 -0500

    ultimas modificaciones archivo index.js

--- trabajando_props/src/index.js
+++ trabajando_props/src/index.js
@@ -2,18 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const Hello = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>Hello {props.name} {props.lastname}</p>
+      <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
   )
 }
 
 const App = () => {
+  const name = 'Miguel'
+  const age = 10
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello name = 'Miguel' lastname='Guerrero Ibarra'/>
+      <Hello name = {name} age = {age + 28}/>
     </div>
   )
 }
