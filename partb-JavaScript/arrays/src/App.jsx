26255b7f936062491be15f29077eef0cd9ea51c1
eduardoha8585 <eduardoha8585@gmail.com>
2024-01-17 18:41:48 -0500

    añadiendo cambios en los archivos y componente App del proyecto arrays en parte b del curso

--- partb-JavaScript/arrays/src/App.jsx
+++ partb-JavaScript/arrays/src/App.jsx
@@ -4,30 +4,20 @@ import viteLogo from '/vite.svg'
 
 
 const App = () => {
-  const [count, setCount] = useState(0)
+  const t = [1, -1, 3]
+
+  t.push(5)
+
+  console.log(t.length) // se imprime 4
+  console.log(t[1])     // se imprime -1
+
+  t.forEach(value => {
+    console.log(value)  // se imprimen los números 1, -1, 3, 5 cada uno en su propia línea
+  })                
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
