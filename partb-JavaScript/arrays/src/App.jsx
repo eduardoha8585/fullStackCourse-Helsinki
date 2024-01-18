2f52f0e860401b0fd6bcd1484a0b261ad5af033b
eduardoha8585 <eduardoha8585@gmail.com>
2024-01-17 20:20:42 -0500

    Añadiendo modificaciones al archivo app.jsx del proyecto arrays perteneciente a la parte b del curso

--- partb-JavaScript/arrays/src/App.jsx
+++ partb-JavaScript/arrays/src/App.jsx
@@ -6,10 +6,16 @@ import viteLogo from '/vite.svg'
 const App = () => {
   const t = [1, -1, 3]
 
-  t.push(5)
+  const t2 = t.concat(5)
 
-  console.log(t.length) // se imprime 4
-  console.log(t[1])     // se imprime -1
+  console.log("Elementos array t", t)
+  console.log("Elementos array t2", t2)
+
+  //t.push(5)
+
+  console.log("Longitud t", t.length) // se imprime 4
+  console.log("longitud t2", t2.length)
+  //console.log(t[1])     // se imprime -1
 
   t.forEach(value => {
     console.log(value)  // se imprimen los números 1, -1, 3, 5 cada uno en su propia línea
