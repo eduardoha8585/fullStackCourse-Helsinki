0ee07ec509aeffb22557c97edfd854072c0579a0
eduardoha8585 <eduardoha8585@gmail.com>
2024-01-24 19:25:56 -0500

    añadiendo cambios a app.jsx del proyecto array perteneciente a parb-JavaScript

--- partb-JavaScript/arrays/src/App.jsx
+++ partb-JavaScript/arrays/src/App.jsx
@@ -6,10 +6,18 @@ import viteLogo from '/vite.svg'
 const App = () => {
   const t = [1, -1, 3]
 
+  /*Utilizando concat para no modificar el objeto(Array) original (t), debido a que react usa tecnicas
+  de programacion funcional y una caracteristica de la programacion funcional es la inmutabilidad de las
+  estructuras de datos*/
   const t2 = t.concat(5)
 
+  //Transformando el objeto(array original t) en algo diferente con map
   const m1 = t.map(value => value * 2)
   const m2 = t.map(value => '<li>' + value + '<li>')
+
+  /*Desestructuracion del objeto original(Array t), asignacion de cada elemento a una variable en este caso
+  primer valor a first, segundo valor a second y los restantes a un array propio llamado rest*/
+  const [first, second, ...rest] = t
 
   console.log("Elemento m2", m2)
 
