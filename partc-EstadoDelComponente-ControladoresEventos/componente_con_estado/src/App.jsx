5593d92bc387e4a2d754989abd7a7350f6b42874
eduardoha8585 <eduardoha8585@gmail.com>
2024-02-07 16:49:28 -0500

    añadiendo proyecto componente_con_estado a partc-Estado.......

---
+++ partc-EstadoDelComponente-ControladoresEventos/componente_con_estado/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+const App = () => {
+  
+  //Agregando state al componente inicializando en cero (0)
+  const [counter, setCounter] = useState(0)
+
+  setTimeout(
+    () => setCounter(counter + 1), 1000
+  )
+
+  return (
+    <>{counter}</>
+  )
+}
+
+export default App
