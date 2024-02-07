bc9c4620dcf5cc1ac2f60848c2a6ed266db67e37
eduardoha8585 <eduardoha8585@gmail.com>
2024-02-07 15:17:57 -0500

    añadiendo proyecto reenderizando_pagina en partc......

---
+++ partc-EstadoDelComponente-ControladoresEventos/reenderizando_pagina/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      Hello
+    </>
+  )
+}
+
+export default App
