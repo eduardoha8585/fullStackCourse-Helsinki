9d899db474271459bc72b31426a3625802573d2d
eduardoha8585 <eduardoha8585@gmail.com>
2024-02-08 18:10:44 -0500

    Añadiendo cambios a proyecto manejo de enventos

--- partc-EstadoDelComponente-ControladoresEventos/manejo_de_eventos/src/App.jsx
+++ partc-EstadoDelComponente-ControladoresEventos/manejo_de_eventos/src/App.jsx
@@ -2,12 +2,26 @@ import { useState } from 'react'
 
 
 const App = () =>  {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(0)
+
+  const handleClick = () =>{
+   console.log("Clicked")
+  }
 
   return (
-    <>
-      hello
-    </>
+    <div>
+      <div>{counter}</div>
+      {/*LOS SIGUIENTES DOS BOTONES NO CAMBIAN EL ESTADO DEL STATE DE LA PAGINA POR LO TANTO
+      NO SE VUELVE A CARGAR LA PAGINA*/}
+      {/*Llamando a la funcion handleClick en el evento onclick del boton*/}   
+      <button onClick={handleClick}>plus</button> 
+      {/*Construyendo la funcion en el evento onclick del boton sin tener que llamar a una funcion
+      previamente programada*/}   
+      <button onClick={() => console.log("Clicked function two")}>plus 2</button>
+
+      <button onClick={() => setCounter(counter + 1)}>Boton que cambia el state</button>
+      <button onClick={() => setCounter(0)}>Boton que reestablece el state</button>
+    </div>
   )
 }
 
