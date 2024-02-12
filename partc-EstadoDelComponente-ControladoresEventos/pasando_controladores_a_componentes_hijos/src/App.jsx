faa01863880ceafdaa8ff0611f6caa24d6e1ac82
eduardoha8585 <eduardoha8585@gmail.com>
2024-02-12 15:47:25 -0500

    añadiendo cambios a proyecto pasando_controladores_a_componentes_hijos de partc-EstadoDel.................

---
+++ partc-EstadoDelComponente-ControladoresEventos/pasando_controladores_a_componentes_hijos/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value ', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before ', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => {
+    console.log('decreasing, value before ', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before ', counter)
+    setCounter(0)
+  }
+
+  return (
+    <div>
+      <Display counter = {counter} />
+      <Button onClick = {increaseByOne} text = 'Plus' />
+      <Button onClick = {decreaseByOne} text = 'Minus' />
+      <Button onClick = {setToZero} text = 'Zero' />
+    </div>
+  )
+}
+
+export default App
