06def43bedf7ba66f64610ebe1e66a29f81e28ee
eduardoha8585 <eduardoha8585@gmail.com>
2024-02-12 14:59:27 -0500

    Añadiendo botones a proyecto manejo_de_eventos en partc-Esta.........

--- partc-EstadoDelComponente-ControladoresEventos/manejo_de_eventos/src/App.jsx
+++ partc-EstadoDelComponente-ControladoresEventos/manejo_de_eventos/src/App.jsx
@@ -8,6 +8,9 @@ const App = () =>  {
    console.log("Clicked")
   }
 
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
+
   return (
     <div>
       <div>{counter}</div>
@@ -15,12 +18,15 @@ const App = () =>  {
       NO SE VUELVE A CARGAR LA PAGINA*/}
       {/*Llamando a la funcion handleClick en el evento onclick del boton*/}   
       <button onClick={handleClick}>plus</button> 
+
       {/*Construyendo la funcion en el evento onclick del boton sin tener que llamar a una funcion
       previamente programada*/}   
       <button onClick={() => console.log("Clicked function two")}>plus 2</button>
 
       <button onClick={() => setCounter(counter + 1)}>Boton que cambia el state</button>
       <button onClick={() => setCounter(0)}>Boton que reestablece el state</button>
+      <button onClick={increaseByOne}>increaseByOne</button>
+      <button onClick={setToZero}>setToZero</button>
     </div>
   )
 }
