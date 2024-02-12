1b103353b2404e34894b87ae8234c5330262b55e
eduardoha8585 <eduardoha8585@gmail.com>
2024-02-12 17:30:04 -0500

    Añadiendo cambios a proyecto estado_complejo

--- partd-UnEstadoMasComplejo-DepurandoAppsReact/estado_complejo/src/App.jsx
+++ partd-UnEstadoMasComplejo-DepurandoAppsReact/estado_complejo/src/App.jsx
@@ -2,15 +2,21 @@ import { useState } from 'react'
 
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+ 
+  const [clicks, setClicks] = useState({
+    left:0, right:0
+  })
+
+  const handleLeftClick = () => setClicks({...clicks, left: clicks.left + 1})
+
+  const handleRightClick = () => setClicks({...clicks, right: clicks.right + 1})
 
   return (
       <div>
-        {left}
-        <button onClick={() => setLeft(left + 1)}>left</button>
-        <button onClick={() => setRight(right + 1)}>right</button>
-        {right}
+        {clicks.left}
+        <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>
+        {clicks.right}
       </div>
   )
 }
