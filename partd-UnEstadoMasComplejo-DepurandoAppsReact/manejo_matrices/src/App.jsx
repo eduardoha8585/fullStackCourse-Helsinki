5876d3074939a33449cab2dc855a9ea1fa057dc2
eduardoha8585 <eduardoha8585@gmail.com>
2024-02-15 18:44:50 -0500

    nuevos cambios en project manejo_matrices

--- partd-UnEstadoMasComplejo-DepurandoAppsReact/manejo_matrices/src/App.jsx
+++ partd-UnEstadoMasComplejo-DepurandoAppsReact/manejo_matrices/src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [count, setCount] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClicks = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClicks = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
   return (
-
       <div>
-        hello
+        {left}
+        <button onClick={handleLeftClicks}>left</button>
+        <button onClick={handleRightClicks}>right</button>
+        {right}
+        <p>{allClicks.join(' ')}</p>
       </div>
-      
   )
 }
 
