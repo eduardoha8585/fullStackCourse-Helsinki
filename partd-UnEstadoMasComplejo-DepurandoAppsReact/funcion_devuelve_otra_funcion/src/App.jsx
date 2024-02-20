0be1fa675bc13f7642e925328a9365082d86e532
eduardoha8585 <eduardoha8585@gmail.com>
2024-02-20 15:55:16 -0500

    Añadiendo cambios a proyecto funcion_devuelve_otra_funcion

--- partd-UnEstadoMasComplejo-DepurandoAppsReact/funcion_devuelve_otra_funcion/src/App.jsx
+++ partd-UnEstadoMasComplejo-DepurandoAppsReact/funcion_devuelve_otra_funcion/src/App.jsx
@@ -1,34 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [value, setValue] = useState(10)
 
+  const hello = (who) => () => {
+      console.log('Hello', who)
+  }
+
+  //Funcion encargada de actualizar el estado
+  const setToValue = (newValue) => {
+    console.log('Value now', newValue)
+    setValue(newValue)
+  }
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div>
+      {value}
+      <p>
+        <button onClick={hello('World')}>Button</button>
+        <button onClick={hello('React')}>Button</button>
+        <button onClick={hello('Function')}>Button</button>
+      </p>      
+
+     
+        <p>BOTONES PARA EL CAMBIO DE ESTADO</p>
+        <button onClick={() => setToValue(1000)}>
+          Thousand
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        <button onClick={() => setToValue(0)}>
+          Reset
+        </button>
+        <button onClick={() => setToValue(value + 1)}>
+          increment
+        </button>
+      
+    </div>
   )
 }
 
