3c050e66cb84bc6d8872e055271d2737f17deeba
eduardoha8585 <eduardoha8585@gmail.com>
2024-02-27 17:35:01 -0500

    cambios en ejercicio 1.6 a 1.14 hasta ejercicio 1.6

--- partd-UnEstadoMasComplejo-DepurandoAppsReact/ejercicios_1.6_1.14/src/App.jsx
+++ partd-UnEstadoMasComplejo-DepurandoAppsReact/ejercicios_1.6_1.14/src/App.jsx
@@ -1,51 +1,49 @@
 import { useState } from 'react'
+import ReactDOM from 'react-dom';
 
-const Button = (props) =>{
-  return(
+
+
+
+const Button = (props) => {
+  return (
     <button onClick={props.handleClick}>{props.text}</button>
   )
 }
 
 //Funcion para mostrar estadisticas
-const Statistics = (props) =>{
-  const {good, neutral, bad} = props
-  const totalComments = good+neutral+bad
+const Statistics = (props) => {
+  const { good, neutral, bad } = props
+  const totalComments = good + neutral + bad
+  const tabla = document.getElementById('TablaOne')
 
-  if((totalComments) === 0){
-    return(
+  if ((totalComments) === 0) {
+    return (
       <div>no feedback given</div>
     )
+  }else{
+    tabla.style.display = 'table'
   }
 
   return (
     <div>
-      {/*
-      <p><h1>statistics</h1></p>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {good + neutral + bad}</div>
-      <div>average {((good*1) + (neutral*0) + (bad*-1))/totalComments}</div>
-      <div>positive {(good / (totalComments))*100}%*/}
-      <StatisticsLine text='good' value = {good} />
-      <StatisticsLine text='neutral' value = {neutral} />
-      <StatisticsLine text='bad' value = {bad} />
-      <StatisticsLine text='all' value = {good + neutral + bad} />
-      <StatisticsLine text='average' value = {((good*1) + (neutral*0) + (bad*-1))/totalComments} />
-      <StatisticsLine text='positive' value = {(good / (totalComments))*100} />
-      
-      </div> 
+     
+      <StatisticsLine text='good' value={good} />
+      <StatisticsLine text='neutral' value={neutral} />
+      <StatisticsLine text='bad' value={bad} />
+      <StatisticsLine text='all' value={good + neutral + bad} />
+      <StatisticsLine text='average' value={((good * 1) + (neutral * 0) + (bad * -1)) / totalComments} />
+      <StatisticsLine text='positive' value={(good / (totalComments)) * 100} />
+
+    </div>
   )
 }
 
-const StatisticsLine = (props) =>{
-  const {text, value} = props
-
-  return(
-    <div>
-      {text} {value}
-    </div>
-  )
+const StatisticsLine = (props) => {
+  const { text, value } = props
+  
+  const fila = document.getElementById(text)
+  fila.cells[1].innerHTML = value  
+  
 }
 
 const App = () => {
@@ -64,9 +62,9 @@ const App = () => {
     <div>
       <h1>give feedback</h1>
       <p>
-        <Button handleClick = {setToGood} text = "good" />
-        <Button handleClick={setToNeutral} text = "neutral" />
-        <Button handleClick={setToBad} text ="bad" />
+        <Button handleClick={setToGood} text="good" />
+        <Button handleClick={setToNeutral} text="neutral" />
+        <Button handleClick={setToBad} text="bad" />
       </p>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
